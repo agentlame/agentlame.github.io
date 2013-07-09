967ee9446f1e26546637dea0e4588606687e2c2d
Neil Layne <n.layne@gmail.com>
2013-07-08 20:32:46 -0400

    Update tbnote.js

--- toolbox/tbnote.js
+++ toolbox/tbnote.js
@@ -2,7 +2,7 @@
   var note = {
       text: 'This is a sample ToolBox notification.  Click on it to see how it is done.',
       link: 'https://github.com/agentlame/agentlame.github.io/edit/master/toolbox/tbnote.js',
-      id: 1000
+      id: 1001
   };
   if (TBUtils) {
       TBUtils.showNote(note);
