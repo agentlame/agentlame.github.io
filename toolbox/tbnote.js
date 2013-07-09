96128e04084b5cd04cf5d5dad6ce16ee03bbb0a0
Neil Layne <n.layne@gmail.com>
2013-07-09 01:44:06 -0400

    Update tbnote.js

--- toolbox/tbnote.js
+++ toolbox/tbnote.js
@@ -1,6 +1,6 @@
 (function() {
   var note = {
-      text: 'Be sure to check-out the new Toolbox Chrome extension.<br><br>Click anywhere to continue.',
+      text: 'Be sure to check-out the new Toolbox Chrome extension.<br>Click anywhere to continue.',
       link: 'http://www.reddit.com/r/toolbox/comments/1hplt9/beta_moderator_toolbox_extension/',
       id: 1000
   };
