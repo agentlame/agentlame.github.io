e8ef24ce9a077befa8615eaaed9c24cd9ad4ffbd
Neil Layne <n.layne@gmail.com>
2013-07-08 20:09:15 -0400

    Create tbnote.js

---
+++ toolbox/tbnote.js
@@ -0,0 +1,10 @@
+(function() {
+  var note = {
+      text: 'testText',
+      link: 'https://github.com/agentlame/agentlame.github.io/new/master/toolbox',
+      id: 1000
+  }
+  if (TBUtils) {
+      TBUtils.showNote(note);
+  }
+})();
